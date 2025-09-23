5b3558f7619f4bc5398a3d802a4c62665904243a
Anatoly <anatoly2678@hmail.com>
2025-09-24 00:21:15 +0700

    ++

--- obj/Debug/net9.0/BookMoney.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookMoney.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMoney")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959dd163e95a191d372839a30e338904ec7ceccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMoney")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMoney")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
